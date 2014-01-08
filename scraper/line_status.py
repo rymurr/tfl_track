4d294cb191fbd2696bf9a92ec6270cb713a2bb5c
Ryan Murray <rymurr@gmail.com>
2014-01-08 23:09:10 +0000

    finished moving stuff around

--- scraper/line_status.py
+++
@@ -1,9 +0,0 @@
-import base
-
-BASE_DIR='data/xml/line_status/'
-SUMMARY_URL = 'http://cloud.tfl.gov.uk/TrackerNet/LineStatus'
-
-class LineStatus(base.Base):
-    def __init__(self):
-        super(LineStatus, self).__init__(SUMMARY_URL, BASE_DIR)
-
