ebcf1907224de8f39a4bab8cffcfe38fa467d994
Ubuntu <ubuntu@ip-172-31-39-24.ec2.internal>
2014-02-03 20:49:43 +0000

    updating base scraper

--- app/scraper/base.py
+++ app/scraper/base.py
@@ -5,11 +5,13 @@ import tarfile
 
 from cStringIO import StringIO
 from logbook import Logger
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+from settings import AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID
 
 from sqsController import sendToSqs
 from data import URLS
 
-TAR_DIR='data/tarballs/'
 log = Logger(__name__)
 class Base(object):
 
@@ -17,27 +19,33 @@ class Base(object):
         return sendToSqs(filenames, name)
 
     def __call__(self):
-        prefix = os.path.expanduser(TAR_DIR)
-        checkBase(prefix)
-        filename = prefix + datetime.datetime.today().strftime('%Y%m%d-%H%M%S')+'.tar.bz2'
+        filename = datetime.datetime.today().strftime('%Y%m%d-%H%M%S')+'.tar.bz2'
         log.info('Writing to tarfile: {0}'.format(filename))
         rs = (grequests.get(url) for url in URLS)
-        with tarfile.open(filename,'w:bz2') as tar:
-            [addToTar(tar, r.text[3:],'{0}.xml'.format(i)) for i,r in enumerate(grequests.map(rs))]
+        tf = StringIO()
+        with tarfile.open(mode='w:bz2',fileobj=tf) as tar:
+            [addToTar(tar, r.text[3:].encode('ascii','ignore'),'{0}.xml'.format(i)) for i,r in enumerate(grequests.map(rs))]
         log.info('Done base fetch, fetched {0} files'.format(len(URLS)))
-        return self.sendToSqs([filename], 'tfl_queue_tar')
- 
+        tf.seek(0)
+        return uploadToS3(tf, filename)
+
+def uploadToS3(fobj, filename):
+    log.debug('Filename is: {0}'.format(filename))
+    conn = S3Connection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    bucket = conn.get_bucket('0nk38gf20cct6vytbg02_tfl_data')
+    try:
+        key = Key(bucket)
+        key.key = 'xml/'+filename
+        log.debug('Sending file {0} with key {1}'.format(filename, key.key))
+        key.set_contents_from_file(fobj)
+        sendToSqs([filename], 'tfl_queue_parse')
+    except:
+        log.exception('Unable to upload file {0}'.format(filename))
+
 def addToTar(tar, data, name):
     info = tarfile.TarInfo(name)
     info.size = len(data)
     tar.addfile(info, StringIO(data))
-
-def checkBase(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-    else:
-        assert os.path.isdir(path)
-
 
 if __name__ == '__main__':
     Base()()
